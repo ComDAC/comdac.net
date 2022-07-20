f7eb0d6b6a51af61f194bf8161eb4c050a7bdb29
David Chappell <dchappell@sirokygroup.com>
2022-07-20 16:45:10 -0400

    three js updates

---
+++ scripts/phase3.js
@@ -0,0 +1,221 @@
+﻿import * as site from "./site.js";
+import * as stats from "./stats.js";
+
+class page {
+    stats;
+    gl;
+    shaderProgram;
+    textureFiles = ["../images/db1.png","../images/db2.png","../images/db3.png","../images/db4.png","../images/db5.png","../images/db6.png","../images/db7.png"];
+    glTexture = [7];
+    ballMul = 20;
+    balls = [];
+    lastFrame = 0;
+
+    dom = {
+        divCanvas: null,
+        cnvs: null,
+        vertexShader: null,
+        fragmentShader: null
+    };
+
+    createShader(gl, type, source) {
+        let shader = gl.createShader(type);
+
+        gl.shaderSource(shader, source);
+        gl.compileShader(shader);
+
+        let success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+
+        if (success) {
+          return shader;
+        }
+       
+        console.log(gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+    }
+
+    createProgram(gl, vertexShader, fragmentShader) {
+        let program = gl.createProgram();
+
+        gl.attachShader(program, vertexShader);
+        gl.attachShader(program, fragmentShader);
+        gl.linkProgram(program);
+
+        let success = gl.getProgramParameter(program, gl.LINK_STATUS);
+
+        if (success) {
+          return program;
+        }
+
+        console.log(gl.getProgramInfoLog(program));
+        gl.deleteProgram(program);
+    }
+
+    isPowerOf2(value) {
+        return (value & (value - 1)) == 0;
+    }
+
+    loadTexture(gl, image) {
+        const texture = gl.createTexture();
+    
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+        if (this.isPowerOf2(image.width) && this.isPowerOf2(image.height)) {
+            gl.generateMipmap(gl.TEXTURE_2D);
+        } else {
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        }
+    
+        return texture;
+    }
+
+    initBalls() {
+        const totalBalls = this.textureFiles.length * this.ballMul;
+
+        for(let i=0; i<totalBalls; i++) {
+            this.balls.push({
+                x: Math.random() * this.gl.canvas.width,
+                y: Math.random() * this.gl.canvas.height,
+                xvel: (Math.random() - 0.5) * 0.7,
+                yvel: (Math.random() - 0.5) * 0.7
+            });
+        }
+    }
+
+    processBalls(time) {
+        this.balls.forEach(b => {
+            b.x += b.xvel * time;
+            b.y += b.yvel * time;
+            
+            if (((b.x < 0) && (b.xvel < 0)) || ((b.x > this.gl.canvas.width) && (b.xvel > 0))) {
+                b.xvel *= -1;
+                b.x += b.xvel * (time / 1000);
+            }            
+            
+            if (((b.y < 0) && (b.yvel < 0)) || ((b.y > this.gl.canvas.height) && (b.yvel > 0))) {
+                b.yvel *= -1;
+                b.y += b.yvel * (time / 1000);
+            }
+        });
+    }
+
+    updatePointsArray(gl, points, shaderProgram, name) {
+        const glBuffer = gl.createBuffer();
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, glBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, points, gl.DYNAMIC_DRAW);
+
+        const loc = gl.getAttribLocation(shaderProgram, name);
+
+        gl.enableVertexAttribArray(loc);
+        gl.vertexAttribPointer(loc, 2, gl.FLOAT, false, 0, 0);
+    }
+
+    loop = (ct) => {
+        requestAnimationFrame(this.loop);
+
+        this.stats.begin();
+
+        const ms = ct - this.lastFrame;
+        this.lastFrame = ct;
+        
+        this.processBalls(ms);
+
+        let points = new Float32Array(this.balls.length * 2);
+
+        this.balls.forEach((b, i) => {
+            points[i*2] = Math.round(b.x);
+            points[i*2+1] = Math.round(b.y);
+        });
+        
+        this.updatePointsArray(this.gl, points, this.shaderProgram, "spritePosition");
+
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+
+        for (let i =0; i<this.glTexture.length; i++) {            
+            this.gl.bindTexture(this.gl.TEXTURE_2D, this.glTexture[i]);
+
+            this.gl.drawArrays(this.gl.POINTS, i * this.ballMul, this.ballMul);
+        }
+
+        this.stats.end();
+    } 
+
+    initTextures(done) {
+        var loadCounter = 0;
+
+        this.textureFiles.forEach((tf, i) => {
+            loadCounter++;
+
+            let img = new Image();
+            img.src = tf;
+
+            img.onload = () => {
+                this.glTexture[i] = this.loadTexture(this.gl, img);
+
+                loadCounter--;
+                if (loadCounter === 0) {
+                    done();
+                }
+            };
+        });
+    }
+
+    init(done) {
+        let vertexShaderSource = this.dom.vertexShader.text;
+        let fragmentShaderSource = this.dom.fragmentShader.text;
+        
+        let vertexShader = this.createShader(this.gl, this.gl.VERTEX_SHADER, vertexShaderSource);
+        let fragmentShader = this.createShader(this.gl, this.gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+        this.shaderProgram = this.createProgram(this.gl, vertexShader, fragmentShader);
+
+        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+
+        this.gl.clearColor(0, 0, 0, 1);
+
+        this.gl.useProgram(this.shaderProgram);
+
+        this.gl.uniform2f(this.gl.getUniformLocation(this.shaderProgram, "screenSize"), this.gl.canvas.width, this.gl.canvas.height);
+
+        this.gl.useProgram(this.shaderProgram);
+
+        this.gl.enable(this.gl.BLEND);
+        this.gl.blendFunc(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA);   
+
+        this.initBalls();
+
+        this.initTextures(done);
+    }
+    
+    //-- entry points ----------------------------
+
+    onLoad = () => {
+        this.stats = stats.init();
+ 
+        this.gl = this.dom.cnvs.getContext("webgl");
+
+        this.gl.canvas.width = this.dom.divCanvas.clientWidth;
+        this.gl.canvas.height = this.dom.divCanvas.clientHeight;
+
+        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+
+        this.init(() => {
+            requestAnimationFrame(this.loop);
+        });
+    }
+
+    onResize = () => {
+        this.gl.canvas.width = this.dom.divCanvas.clientWidth;
+        this.gl.canvas.height = this.dom.divCanvas.clientHeight;
+        
+        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+
+        this.gl.uniform2f(this.gl.getUniformLocation(this.shaderProgram, "screenSize"), this.gl.canvas.width, this.gl.canvas.height);
+    }
+}
+
+site.registerPage(page);
--- scripts/phase4.js
+++ scripts/phase4.js
@@ -1,433 +1,194 @@
-﻿'use strict';
+﻿import * as site from "./site.js";
+import * as stats from "./stats.js";
+import * as THREE from "three";
+import { GLTFLoader } from "GTFLoader";
+import { TrackballControls } from './TrackballControls.js';
 
-(function () {
-    var camera, scene, renderer;
-    var controls;
+class page {
+    stats;
 
-    var loadingCount = 0;
+    camera;
+    scene;
+    renderer;
+    controls;
 
-    var deltaTime, lastFrameTime;
+    lastFrameTime = 0;
 
-    var awing, light, ambient;
-    var awingpivot, awingobj, particleSpinner;
+    awing;
+    awingpivot;
+    awingobj;
+    awingEnginePosCords = [-4.7, 2.5, 0, -4.7,-2.5, 0];
+    awingEnginePos = [2];
 
-    var particleShader = {};
-    var particleMaterial;
-    var particleGeometry;
-    var particlePosStart;
-    var particleVelCol;
-    var particleSize = 480.0;
-    var particleCount = 10000;
-    var particleSystem;
-    var particleCursor = 0;
-    var particles;
+    light;
 
-    var stats;
+    pointLights = [2];
 
-    var mpi = Math.PI;
-    var msin = Math.sin;
-    var mr = Math.random;
-    var msqrt = Math.sqrt;
+    particleSpinner;
 
-    var currentTime;
+    init() {
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: true
+        });
 
-    var engineVel;
+        this.stats = stats.init();
 
-    //render loop variables.
-    var mw, psr, sp0, sp1, ep0, ep1, sv0, sv1, pl0, pl1;
+        let loadCounter = 0;
 
-    //particle system variables;
-    var pi, p, pp, av, ppv, teul, tm;
+        loadCounter++;        
+        new GLTFLoader().load("../models/awing.glb", (glb) => {
+            console.log("load");
+            this.awing = glb.scene;
 
-    var pointCount = 4000;
-    var pointPositions;
-    var pointAlphas;
-    var pointGeometry;
-    var pointProperties = [];
-    var pointPool = [];
+            loadCounter--;
 
-    function hasWebGL() {
-        try {
-            return !!window.WebGLRenderingContext && !!document.createElement('canvas').getContext('experimental-webgl');
-        } catch (e) {
-            return false;
-        }
-    }
-
-    function init() {
-        var ok = false;
-
-        if (hasWebGL()) {
-            renderer = new THREE.WebGLRenderer({
-                antialias: true
-            });
-
-            stats = new Stats();
-            stats.domElement.style.position = "absolute";
-            stats.domElement.style.top = "10px";
-            stats.domElement.style.right = "10px";
-            document.body.appendChild(stats.domElement);
-
-            ok = true;
-        } else {
-            document.getElementById("loadingMessage").style.display = "none";
-
-            document.getElementById("canvaserror").style.display = "block";
-        }
-
-        if (ok) {
-            loading();
-            new THREE.TextureLoader().load("../images/particle-white.png", function (texture) {
-                particleShader = {
-                    uniforms: {
-                        texture: { type: "t", value: texture },
-                        size: { type: "f", value: particleSize },
-                        time: { type: "f", value: 0.0 }
-                    },
-                    vertexShader: document.getElementById("particleVertexShader").innerHTML,
-                    fragmentShader: document.getElementById("particleFragmentShader").innerHTML,
-                    blending: THREE.AdditiveBlending,
-                    transparent: true,
-                    depthWrite: false
-                }
-
-                loaded();
-            });
-
-            loading();
-            new THREE.JSONLoader().load("../models/awing.js", function (geometry, materials) {
-                var material = new THREE.MeshFaceMaterial(materials);
-                awing = new THREE.Mesh(geometry, material);
-
-                loaded();
-            }, "../models/awing");
-        }
-    }
-
-    function loadComplete() {
-        tm = new THREE.Matrix4();
-
-        ep0 = new THREE.Vector3();
-        ep1 = new THREE.Vector3();
-
-        engineVel = new THREE.Vector3();
-
-        sp0 = new THREE.Vector3();
-        sp1 = new THREE.Vector3();
-        sv0 = new THREE.Vector3();
-        sv1 = new THREE.Vector3();
-
-        teul = new THREE.Euler(0, 0, 0, "XYZ");
-
-        awingpivot = new THREE.Object3D();
-        awingobj = new THREE.Object3D();
-        particleSpinner = new THREE.Object3D();
-
-        pointPositions = new Float32Array(pointCount * 3);
-        pointAlphas = new Float32Array(pointCount);
-
-        scene = new THREE.Scene();
-
-        camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
-        camera.position.z = 3000;
-
-        controls = new THREE.TrackballControls(camera);
-        
-        controls.rotateSpeed = 1.0;
-        controls.zoomSpeed = 1.2;
-        controls.panSpeed = 0.2;
-        
-        controls.noZoom = false;
-        controls.noPan = false;
-        
-        controls.staticMoving = false;
-        controls.dynamicDampingFactor = 0.3;
-        
-        controls.minDistance = 2000;
-        controls.maxDistance = 8000;
-        
-        controls.keys = [65, 83, 68];
-
-        //init particle system here.
-        particles = new THREE.Geometry();
-        particleGeometry = new THREE.BufferGeometry();
-
-        var particleVertices = new Float32Array(particleCount * 3); // position
-        var particlePositionsStartTime = new Float32Array(particleCount * 4); // position
-        var particleVelColSizeLife = new Float32Array(particleCount * 4);
-
-        for (var i = 0; i < particleCount; i++) {
-            particlePositionsStartTime[i * 4 + 0] = 100; //x
-            particlePositionsStartTime[i * 4 + 1] = 0; //y
-            particlePositionsStartTime[i * 4 + 2] = 0.0; //z
-            particlePositionsStartTime[i * 4 + 3] = 0.0; //startTime
-
-            particleVertices[i * 3 + 0] = 0; //x
-            particleVertices[i * 3 + 1] = 0; //y
-            particleVertices[i * 3 + 2] = 0.0; //z
-
-            particleVelColSizeLife[i * 4 + 0] = decodeFloat(128, 128, 0, 0); //vel
-            particleVelColSizeLife[i * 4 + 1] = decodeFloat(0, 254, 0, 254); //color
-            particleVelColSizeLife[i * 4 + 2] = 1.0; //size
-            particleVelColSizeLife[i * 4 + 3] = 0.0; //lifespan
-        }
-
-        particleGeometry.addAttribute('position', new THREE.BufferAttribute(particleVertices, 3));
-        particleGeometry.addAttribute('particlePositionsStartTime', new THREE.BufferAttribute(particlePositionsStartTime, 4).setDynamic(true));
-        particleGeometry.addAttribute('particleVelColSizeLife', new THREE.BufferAttribute(particleVelColSizeLife, 4).setDynamic(true));
-
-        particlePosStart = particleGeometry.getAttribute('particlePositionsStartTime')
-        particleVelCol = particleGeometry.getAttribute('particleVelColSizeLife');
-
-        particleMaterial = new THREE.ShaderMaterial(particleShader);
-
-        particleSystem = new THREE.Points(particleGeometry, particleMaterial);
-        particleSystem.sortParticles = true;
-        scene.add(particleSystem);
-
-        //init other stuff here
-
-        for (var i = 0, i3 = 0; i < pointCount; i++, i3 += 3) {
-            pointAlphas[i] = 0.3;
-
-            pointPositions[i3 + 0] = Number.POSITIVE_INFINITY;
-            pointPositions[i3 + 1] = Number.POSITIVE_INFINITY;
-            pointPositions[i3 + 2] = Number.POSITIVE_INFINITY;
-
-            pointProperties.push({ "ttl": 0, "max": 0, "vel": new THREE.Vector3(), "alive": false });
-
-            pointPool.push(i);
-        }
-
-
-        pointGeometry = new THREE.BufferGeometry();
-
-        pointGeometry.addAttribute('position', new THREE.BufferAttribute(pointPositions, 3));
-        pointGeometry.addAttribute('alpha', new THREE.BufferAttribute(pointAlphas, 1));
-
-        var pMaterial = new THREE.ShaderMaterial(particleShader);
-
-        particleSystem = new THREE.Points(pointGeometry, pMaterial);
-        particleSystem.sortParticles = true;
-        scene.add(particleSystem);
-
-        awingpivot.position.set(0, 0, 0);
-        awingpivot.rotation.y = 1;
-        scene.add(awingpivot);
-
-        awing.scale.set(2.0, 2.0, 2.0);
-        awing.rotation.x = (mpi / 2);
-        awing.rotation.z = -0.04;
-        awingobj.add(awing);
-
-        awingobj.position.x = 700;
-        awingobj.rotation.x = -mpi / 2;
-        awingobj.rotation.z = mpi / 2;
-        awingobj.rotation.y = -0.4;
-        awingpivot.add(awingobj);
-
-        light = new THREE.PointLight(0xFFFFFF);
-        light.position.set(0, 0, 4000);
-        scene.add(light);
-
-        scene.add(particleSpinner);
-
-        pl0 = new THREE.PointLight(0xFF9999);
-        pl1 = new THREE.PointLight(0xFF9999);
-
-        pl0.position.set(1200, 0, 0);
-        pl1.position.set(-1200, 0, 0);
-
-        particleSpinner.add(pl0);
-        particleSpinner.add(pl1);
-
-        particleSpinner.matrixAutoUpdate = false;
-        pl0.matrixAutoUpdate = false;
-        pl1.matrixAutoUpdate = false;
-        awingobj.matrixAutoUpdate = false;
-
-        renderer.setSize(window.innerWidth, window.innerHeight);
-
-        document.body.appendChild(renderer.domElement);
-
-        window.onresize = resize;
-        resize();
-
-        document.getElementById("loadingMessage").style.display = "none";
-
-        lastFrameTime = Date.now();
-
-        //render base variables
-        psr = particleSpinner.rotation;
-
-        //particle base variables
-        //av = particleShader.attributes.alpha.value;
-
-        requestAnimationFrame(animate);
-    }
-
-    function render() {
-        currentTime = Date.now();
-        deltaTime = currentTime - lastFrameTime;
-        lastFrameTime = currentTime;
-
-
-        pointGeometry.attributes.position.needsUpdate = true;
-        pointGeometry.attributes.alpha.needsUpdate = true;
-
-        //update all particles in the system.
-        pi = pointCount;
-        
-        while (pi--) {
-            pp = pointProperties[pi];
-            if (pp.alive) {
-                pp.ttl -= deltaTime;
-        
-                if (pp.ttl < 1) {
-                    pointPositions[(pi * 3) + 0] = Number.POSITIVE_INFINITY;
-                    pointPositions[(pi * 3) + 1] = Number.POSITIVE_INFINITY;
-                    pointPositions[(pi * 3) + 2] = Number.POSITIVE_INFINITY;
-                    pp.alive = false;
-                    pointPool.push(pi);
-                } else {
-                    pointPositions[(pi * 3) + 0] += pp.vel.x * deltaTime;
-                    pointPositions[(pi * 3) + 1] += pp.vel.y * deltaTime;
-                    pointPositions[(pi * 3) + 2] += pp.vel.z * deltaTime;
-                    pointAlphas[pi] = pp.ttl / pp.max * 0.3;
-                }
+            if (loadCounter === 0) {
+                this.loadComplete();
             }
-        }
-
-        //process all objects in the scene.
-        awingpivot.rotation.y += 0.0012 * deltaTime;
-
-        psr.z += 0.001 * deltaTime;
-        psr.y += msin(psr.z / 5) / 24;
-        psr.x += msin(psr.z / 10) / 22;
-
-        //setup particle engines
-
-        ep0.set(-230, 133, 0);
-        ep1.set(-230, -133, 0);
-        engineVel.set(100, 0, 0);
-
-        awingobj.updateMatrix();
-
-        mw = awingobj.matrixWorld;
-        ep0.applyMatrix4(mw);
-        ep1.applyMatrix4(mw);
-        engineVel.applyMatrix4(mw);
-
-        engineVel.setLength(0.4);
-
-        particleEmitter(particles, pointProperties, 0.15, 80, 120, ep0, engineVel, deltaTime, 5);
-        particleEmitter(particles, pointProperties, 0.15, 80, 120, ep1, engineVel, deltaTime, 5);
-
-        sp0.set(0, 0, 0);
-        sp1.set(0, 0, 0);
-        sv0.set(0, -80, 0);
-        sv1.set(0, 80, 0);
-
-        pl0.updateMatrix();
-        pl1.updateMatrix();
-
-        sp0.applyMatrix4(pl0.matrixWorld);
-        sp1.applyMatrix4(pl1.matrixWorld);
-
-        particleSpinner.updateMatrix();
-
-        mw = particleSpinner.matrixWorld;
-        sv0.applyMatrix4(mw);
-        sv1.applyMatrix4(mw);
-
-        sv0.setLength(0.4);
-        sv1.setLength(0.4);
-
-        particleEmitter(particles, pointProperties, mpi * 1.2, 700, 1500, sp0, sv0, deltaTime, 25);
-        particleEmitter(particles, pointProperties, mpi * 1.2, 700, 1500, sp1, sv1, deltaTime, 25);
-
-        renderer.render(scene, camera);
+        });
     }
 
-    function particleEmitter(ps, psp, spray, minlife, maxlife, pos, dir, deltaTime, n) {
-        while (n--) {
-            particlePosStart.array[particleCursor * 4 + 0] = pos.x;
-            particlePosStart.array[particleCursor * 4 + 1] = pos.y;
-            particlePosStart.array[particleCursor * 4 + 2] = pos.z;
-            particlePosStart.array[particleCursor * 4 + 3] = currentTime;
+    makeCube(size, col, x = 0, y = 0, z = 0) {
+        const geometry = new THREE.BoxGeometry( size, size, size );
+        const material = new THREE.MeshBasicMaterial( { color: col } );
+        const cube = new THREE.Mesh( geometry, material );
 
-            //ps.vertices[pi].copy(pos);
-            pp = psp[pi];
-            //ppv = pp.vel.copy(dir);
-    
-            teul.x = (mr() - 0.5) * spray;
-            teul.y = (mr() - 0.5) * spray;
-            teul.z = (mr() - 0.5) * spray;
-    
-            tm.makeRotationFromEuler(teul);
-    
-            ppv.applyMatrix4(tm);
+        cube.position.set(x, y, z);
 
-            particleVelCol.array[particleCursor * 4 + 0] = decodeFloat(ppv.x, ppv.y, ppv.z);
-            particleVelCol.
-    
-            av[pi] = 0.3;
-            pp.max = pp.ttl = mr() * (maxlife - minlife) + minlife;
-            pp.alive = true;
+        return cube;
+    }
 
-            particleCursor++;
+    loadComplete() {
+        this.scene = new THREE.Scene();
+
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+
+        this.camera.position.z = 40;
+
+        this.controls = new TrackballControls(this.camera, this.renderer.domElement);
+        
+        this.controls.rotateSpeed = 18.0;
+        this.controls.zoomSpeed = 1.2;
+        this.controls.panSpeed = 0.2;        
+        this.controls.minDistance = 10;
+        this.controls.maxDistance = 100;
+
+        //initialize objects
+
+        this.awingpivot = new THREE.Object3D();
+        this.awingobj = new THREE.Object3D();
+        for(let i = 0; i < this.awingEnginePosCords.length; i+=3) {  
+            this.awingEnginePos[i] = new THREE.Vector3();
         }
+
+        this.awingpivot.position.set(0, 0, 0);
+        this.awingpivot.rotation.y = 1;
+        this.scene.add(this.awingpivot);
+
+        this.awing.rotation.x = (Math.PI / 2);
+        this.awing.rotation.z = -0.04;
+        this.awingobj.add(this.awing);
+
+        for(let i = 0; i < this.awingEnginePosCords.length; i+=3) {            
+            this.awingobj.add(this.makeCube(2, 0x00FF00, this.awingEnginePosCords[i],this.awingEnginePosCords[i+1],this.awingEnginePosCords[i+2]));
+        }
+
+        this.awingobj.position.x = 20;
+        this.awingobj.rotation.x = -Math.PI / 2;
+        this.awingobj.rotation.z = Math.PI / 2;
+        this.awingobj.rotation.y = -0.4;
+        
+        this.awingpivot.add(this.awingobj);
+
+        this.awingobj.matrixAutoUpdate = false;
+
+        this.light = new THREE.PointLight(0xFFFFFF);
+        this.light.position.set(0, 0, 300);
+
+        this.scene.add(this.light);
+        
+        this.particleSpinner = new THREE.Object3D();
+
+        const lights = [25, -25];
+
+        for(let i=0; i<lights.length; i++) {
+            this.pointLights[i] = new THREE.PointLight(0xFF9999);
+            this.pointLights[i].position.set(lights[i], 0, 0);
+            this.particleSpinner.add(this.pointLights[i]);
+
+            this.particleSpinner.add(this.makeCube(2, 0xFF0000, lights[i], 0, 0));
+        }
+
+        this.particleSpinner.matrixAutoUpdate = false;
+        
+        this.scene.add(this.particleSpinner);
+
+        //initialize viewport
+        
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+        document.body.appendChild(this.renderer.domElement);
+
+        this.onResize();
+
+        requestAnimationFrame(this.animate);
+    }
+
+    render(tm) {
+        const deltaTime = tm - this.lastFrameTime;
+        this.lastFrameTime = tm;
+
+        //do delta stuff here.
+
+        this.awingpivot.rotation.y += 0.0012 * deltaTime;
+
+        this.particleSpinner.rotation.z += 0.001 * deltaTime;
+        this.particleSpinner.rotation.y += Math.sin(this.particleSpinner.rotation.z / 5) / 24;
+        this.particleSpinner.rotation.x += Math.sin(this.particleSpinner.rotation.z / 10) / 22;
+
+        this.awingobj.updateMatrix();
+        this.particleSpinner.updateMatrix();
+
+        for(let i = 0; i < this.awingEnginePosCords.length; i+=3) {            
+            this.awingEnginePos[i].set(this.awingEnginePosCords[i],this.awingEnginePosCords[i+1],this.awingEnginePosCords[i+2]);
+
+            this.awingEnginePos[i].applyMatrix4(this.awingobj.matrixWorld);
+
+            //engine pos now has proper world position for particle system.
+        }
+
+        //render final scene
+        this.renderer.render(this.scene, this.camera);
     }
 
     //--background stuff
-    if (!window.console) console = { log: function () { } };
-
-    window.onload = function () {
-        init();
+    onLoad = () => {
+        this.init();
     }
 
-    function resize() {
-        var width = window.innerWidth;
-        var height = window.innerHeight;
+    onResize = () => {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
 
-        camera.aspect = width / height;
-        camera.updateProjectionMatrix();
+        this.camera.aspect = width / height;
+        this.camera.updateProjectionMatrix();
 
-        controls.handleResize();
+        this.controls.handleResize();
 
-        particleShader.uniforms.size.value = particleSize * (msqrt(width * width + height * height) / 1280.0);
-
-        renderer.setSize(width, height);
+        this.renderer.setSize(width, height);
     }
 
-    function loading() {
-        loadingCount++;
+    animate = (tm) => {
+        requestAnimationFrame(this.animate);
+
+        this.controls.update();
+
+        this.stats.begin();
+
+        this.render(tm);
+
+        this.stats.end();
     }
+}
 
-    function loaded() {
-        loadingCount--;
-        if (loadingCount < 1) loadComplete();
-    }
-
-    function animate() {
-        requestAnimationFrame(animate);
-
-        controls.update();
-
-        render();
-
-        stats.update();
-    }
-
-    //--helper functions
-    var UINT8_VIEW = new Uint8Array(4)
-    var FLOAT_VIEW = new Float32Array(UINT8_VIEW.buffer)
-
-    function decodeFloat(x, y, z, w) {
-        UINT8_VIEW[0] = Math.floor(w)
-        UINT8_VIEW[1] = Math.floor(z)
-        UINT8_VIEW[2] = Math.floor(y)
-        UINT8_VIEW[3] = Math.floor(x)
-        return FLOAT_VIEW[0]
-    }
-})();
+site.registerPage(page);
